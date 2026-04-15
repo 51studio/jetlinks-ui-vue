d6ba45b60bdc77a4636d7c81f9f4835406a07c42
TANG <>
2026-04-15 18:50:14 +0800

    修改应用管理的路由设置

--- src/api/open-api-client.ts
+++ src/api/open-api-client.ts
@@ -2,53 +2,53 @@ import { request } from '@jetlinks-web/core'
 
 // -----------------------------------------------------------------------
 // 应用管理接口
-// 对应后端 /open-api/client 路由
+// 对应后端 /application 路由
 // -----------------------------------------------------------------------
 
 export const queryApiClientList = (data: any) =>
-  request.post<any>('/open-api/client/_query', data)
+  request.post<any>('/application/_query', data)
 
 export const getApiClient = (id: string) =>
-  request.get<any>(`/open-api/client/${id}`)
+  request.get<any>(`/application/${id}`)
 
 export const addApiClient = (data: any) =>
-  request.post<any>('/open-api/client', data)
+  request.post<any>('/application', data)
 
 export const updateApiClient = (id: string, data: any) =>
-  request.put<any>(`/open-api/client/${id}`, data)
+  request.put<any>(`/application/${id}`, data)
 
 export const deleteApiClient = (id: string) =>
-  request.remove<any>(`/open-api/client/${id}`)
+  request.remove<any>(`/application/${id}`)
 
 export const enableApiClient = (id: string) =>
-  request.post<any>(`/open-api/client/${id}/_enable`)
+  request.post<any>(`/application/${id}/_enable`)
 
 export const disableApiClient = (id: string) =>
-  request.post<any>(`/open-api/client/${id}/_disable`)
+  request.post<any>(`/application/${id}/_disable`)
 
 export const generateApiClientKeys = (id: string) =>
-  request.post<any>(`/open-api/client/${id}/_generate-keys`)
+  request.post<any>(`/application/${id}/_generate-keys`)
 
 export const issueApiClientToken = (id: string) =>
-  request.post<any>(`/open-api/client/${id}/_issue-token`)
+  request.post<any>(`/application/${id}/_issue-token`)
 
 export const queryApiClientAccessLog = (id: string, data: any) =>
-  request.post<any>(`/open-api/client/${id}/access-log/_query`, data)
+  request.post<any>(`/application/${id}/access-log/_query`, data)
 
 export const bindApiClientRole = (clientId: string, data: string[]) =>
-  request.post<any>(`/open-api/client/${clientId}/role/_bind`, data)
+  request.post<any>(`/application/${clientId}/role/_bind`, data)
 
 export const unbindApiClientRole = (clientId: string, data: string[]) =>
-  request.post<any>(`/open-api/client/${clientId}/role/_unbind`, data)
+  request.post<any>(`/application/${clientId}/role/_unbind`, data)
 
 export const getApiClientRoles = (clientId: string, data?: any) =>
-  request.post<any>(`/open-api/client/${clientId}/role/_query`, data)
+  request.post<any>(`/application/${clientId}/role/_query`, data)
 
 export const bindApiClientOrg = (clientId: string, data: object) =>
-  request.post<any>(`/open-api/client/${clientId}/org/_bind`, data)
+  request.post<any>(`/application/${clientId}/org/_bind`, data)
 
 export const unbindApiClientOrg = (clientId: string, data: object) =>
-  request.post<any>(`/open-api/client/${clientId}/org/_unbind`, data)
+  request.post<any>(`/application/${clientId}/org/_unbind`, data)
 
 export const getApiClientOrgs = (clientId: string, data?: any) =>
-  request.post<any>(`/open-api/client/${clientId}/org/_query`, data)
+  request.post<any>(`/application/${clientId}/org/_query`, data)
